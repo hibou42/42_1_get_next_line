48985301c6d876bb117df1dc95505aca75e1fe33
Arnaud Schaefer <aschaefe@student.42lausanne.ch>
2022-11-22 11:53:16 +0100

    let's start the project

---
+++ get_next_line.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aschaefe <marvin@42lausanne.ch>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/22 11:35:31 by aschaefe          #+#    #+#             */
+/*   Updated: 2022/11/22 11:49:15 by aschaefe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include "get_next_line.h"
+# include <stdio.h>
+# include <unistd.h>
+
+char	*get_next_line(int fd);
+
+#endif
+
